61ce69646576a8b1f4f54bf396ac0dbd4916df83
Selina Shi <selina@Selinas-MacBook-Air.local>
2023-05-10 16:50:59 -0700

    clean console.log

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -15,6 +15,8 @@ router.get("/", (req, res) => {
       id: user.id,
       email: user.email,
       username: user.username,
+      firstName:user.firstName,
+      lastName:user.lastName
     };
     return res.json({
       user: safeUser,
@@ -58,6 +60,8 @@ router.post("/", validateLogin, async (req, res, next) => {
     id: user.id,
     email: user.email,
     username: user.username,
+    firstName:user.firstName,
+    lastName:user.lastName
   };
 
   await setTokenCookie(res, safeUser);
